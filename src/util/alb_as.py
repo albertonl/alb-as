f619d38350bf0c095d33689c7627a86030d00099
Alberto Navalon <alberto.navalonlillo@gmail.com>
2020-06-02 12:01:56 +0200

    update version in project specifications

--- src/util/alb_as.py
+++ src/util/alb_as.py
@@ -1,4 +1,4 @@
 software_name = "ALB AulesScraper"
 software_code = "ALB-AS"
-software_version = "v0.1alpha"
+software_version = "v0.1.1beta"
 software_repo_url = "https://github.com/albertonl/alb-as"
